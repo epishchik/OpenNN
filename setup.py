4a137dcdb9b0f20aeaae260ca033c7fa7b29527e
Evgenii Pishchik <57502208+Pe4enIks@users.noreply.github.com>
2022-04-03 04:07:33 +0300

    new realease

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, Command
 NAME = 'opennn_pytorch'
 DESCRIPTION = 'Neural Networks library for image classification task.'
 URL = 'https://github.com/Pe4enIks/OpenNN'
-DOWNLOAD_URL = 'https://github.com/Pe4enIks/OpenNN/archive/refs/tags/v0.0.0.zip'
+DOWNLOAD_URL = 'https://github.com/Pe4enIks/OpenNN/archive/refs/tags/v.0.0.1.zip'
 EMAIL = 'evgeniipishchik@mail.ru'
 AUTHOR = 'Evgenii Pishchik'
 REQUIRES_PYTHON = '>=3.6.0'
