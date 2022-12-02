777106a1248f4e21de21eed520ee356463357ae8
Pe4enIks <evgeniipishchik@mail.ru>
2022-12-02 03:46:21 +0300

    resnet refactoring

--- opennn_pytorch/encoder/resnet.py
+++ opennn_pytorch/encoder/resnet.py
@@ -1,4 +1,5 @@
 from torch import nn
+from collections import OrderedDict
 
 
 class ResidualBlock1(nn.Module):
@@ -96,10 +97,22 @@ class Downsample(nn.Module):
         return self.bn(self.conv(x))
 
 
-class ResNet18(nn.Module):
+def _make_layer(block, num_inc_outc):
+    blocks = []
+    idx = 0
+
+    for triple in num_inc_outc:
+        for _ in range(triple[0]):
+            blocks.append((f'block{idx}', block(*triple[1:])))
+            idx += 1
+
+    return nn.Sequential(OrderedDict(blocks))
+
+
+class BaseResNet(nn.Module):
     def __init__(self, inc):
         super().__init__()
-        self.features = 512
+
         self.conv1 = nn.Conv2d(inc,
                                64,
                                kernel_size=(7, 7),
@@ -113,271 +126,107 @@ class ResNet18(nn.Module):
                                     padding=1,
                                     dilation=1,
                                     ceil_mode=False)
-        self.resblock11 = ResidualBlock1(64, 64)
-        self.resblock12 = ResidualBlock1(64, 64)
-        self.resblock21 = ResidualBlock1(64, 128)
-        self.resblock22 = ResidualBlock1(128, 128)
-        self.resblock31 = ResidualBlock1(128, 256)
-        self.resblock32 = ResidualBlock1(256, 256)
-        self.resblock41 = ResidualBlock1(256, 512)
-        self.resblock42 = ResidualBlock1(512, 512)
+
         self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
     def forward(self, x):
-        pr = self.maxpool(self.relu(self.bn1(self.conv1(x))))
-        b1 = self.resblock12(self.resblock11(pr))
-        b2 = self.resblock22(self.resblock21(b1))
-        b3 = self.resblock32(self.resblock31(b2))
-        b4 = self.resblock42(self.resblock41(b3))
-        b5 = self.avgpool(b4)
-        return b5
+        conv = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+
+        l1 = self.layer1(conv)
+        l2 = self.layer2(l1)
+        l3 = self.layer3(l2)
+        l4 = self.layer4(l3)
+
+        avgpool = self.avgpool(l4)
+        return avgpool
 
     def out_features(self):
         return self.features
 
 
-class ResNet34(nn.Module):
+class ResNet18(BaseResNet):
     def __init__(self, inc):
-        super().__init__()
+        super().__init__(inc)
         self.features = 512
-        self.conv1 = nn.Conv2d(inc,
-                               64,
-                               kernel_size=(7, 7),
-                               stride=(2, 2),
-                               padding=(3, 3),
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=3,
-                                    stride=2,
-                                    padding=1,
-                                    dilation=1,
-                                    ceil_mode=False)
-        self.resblock11 = ResidualBlock1(64, 64)
-        self.resblock12 = ResidualBlock1(64, 64)
-        self.resblock13 = ResidualBlock1(64, 64)
-        self.resblock21 = ResidualBlock1(64, 128)
-        self.resblock22 = ResidualBlock1(128, 128)
-        self.resblock23 = ResidualBlock1(128, 128)
-        self.resblock24 = ResidualBlock1(128, 128)
-        self.resblock31 = ResidualBlock1(128, 256)
-        self.resblock32 = ResidualBlock1(256, 256)
-        self.resblock33 = ResidualBlock1(256, 256)
-        self.resblock34 = ResidualBlock1(256, 256)
-        self.resblock35 = ResidualBlock1(256, 256)
-        self.resblock36 = ResidualBlock1(256, 256)
-        self.resblock41 = ResidualBlock1(256, 512)
-        self.resblock42 = ResidualBlock1(512, 512)
-        self.resblock43 = ResidualBlock1(512, 512)
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
-    def forward(self, x):
-        pr = self.maxpool(self.relu(self.bn1(self.conv1(x))))
-        b1 = self.resblock13(self.resblock12(self.resblock11(pr)))
-        b2 = self.resblock24(self.resblock23(
-            self.resblock22(self.resblock21(b1))))
-        b3 = self.resblock36(self.resblock35(self.resblock34(
-            self.resblock33(self.resblock32(self.resblock31(b2))))))
-        b4 = self.resblock43(self.resblock42(self.resblock41(b3)))
-        b5 = self.avgpool(b4)
-        return b5
+        self.layer1 = _make_layer(ResidualBlock1, ((2, 64, 64),))
 
-    def out_features(self):
-        return self.features
+        self.layer2 = _make_layer(ResidualBlock1, ((1, 64, 128),
+                                                   (1, 128, 128)))
+
+        self.layer3 = _make_layer(ResidualBlock1, ((1, 128, 256),
+                                                   (1, 256, 256)))
+
+        self.layer4 = _make_layer(ResidualBlock1, ((1, 256, 512),
+                                                   (1, 512, 512)))
 
 
-class ResNet50(nn.Module):
+class ResNet34(BaseResNet):
     def __init__(self, inc):
-        super().__init__()
-        self.features = 2048
-        self.conv1 = nn.Conv2d(inc, 64, kernel_size=(
-            7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(
-            kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
-        self.resblock11 = ResidualBlock2(64, 64)
-        self.resblock12 = ResidualBlock2(256, 64)
-        self.resblock13 = ResidualBlock2(256, 64)
-        self.resblock21 = ResidualBlock2(256, 128)
-        self.resblock22 = ResidualBlock2(512, 128)
-        self.resblock23 = ResidualBlock2(512, 128)
-        self.resblock24 = ResidualBlock2(512, 128)
-        self.resblock31 = ResidualBlock2(512, 256)
-        self.resblock32 = ResidualBlock2(1024, 256)
-        self.resblock33 = ResidualBlock2(1024, 256)
-        self.resblock34 = ResidualBlock2(1024, 256)
-        self.resblock35 = ResidualBlock2(1024, 256)
-        self.resblock36 = ResidualBlock2(1024, 256)
-        self.resblock41 = ResidualBlock2(1024, 512)
-        self.resblock42 = ResidualBlock2(2048, 512)
-        self.resblock43 = ResidualBlock2(2048, 512)
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        super().__init__(inc)
+        self.features = 512
 
-    def forward(self, x):
-        pr = self.maxpool(self.relu(self.bn1(self.conv1(x))))
-        b1 = self.resblock13(self.resblock12(self.resblock11(pr)))
-        b2 = self.resblock24(self.resblock23(
-            self.resblock22(self.resblock21(b1))))
-        b3 = self.resblock36(self.resblock35(self.resblock34(
-            self.resblock33(self.resblock32(self.resblock31(b2))))))
-        b4 = self.resblock43(self.resblock42(self.resblock41(b3)))
-        b5 = self.avgpool(b4)
-        return b5
+        self.layer1 = _make_layer(ResidualBlock1, ((3, 64, 64),))
 
-    def out_features(self):
-        return self.features
+        self.layer2 = _make_layer(ResidualBlock1, ((1, 64, 128),
+                                                   (3, 128, 128)))
+
+        self.layer3 = _make_layer(ResidualBlock1, ((1, 128, 256),
+                                                   (5, 256, 256)))
+
+        self.layer4 = _make_layer(ResidualBlock1, ((1, 256, 512),
+                                                   (2, 512, 512)))
 
 
-class ResNet101(nn.Module):
+class ResNet50(BaseResNet):
     def __init__(self, inc):
-        super().__init__()
+        super().__init__(inc)
         self.features = 2048
-        self.conv1 = nn.Conv2d(inc, 64, kernel_size=(
-            7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(
-            kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
-        self.resblock11 = ResidualBlock2(64, 64)
-        self.resblock12 = ResidualBlock2(256, 64)
-        self.resblock13 = ResidualBlock2(256, 64)
-        self.resblock21 = ResidualBlock2(256, 128)
-        self.resblock22 = ResidualBlock2(512, 128)
-        self.resblock23 = ResidualBlock2(512, 128)
-        self.resblock24 = ResidualBlock2(512, 128)
-        self.resblock31 = ResidualBlock2(512, 256)
-        self.resblock32 = ResidualBlock2(1024, 256)
-        self.resblock33 = ResidualBlock2(1024, 256)
-        self.resblock34 = ResidualBlock2(1024, 256)
-        self.resblock35 = ResidualBlock2(1024, 256)
-        self.resblock36 = ResidualBlock2(1024, 256)
-        self.resblock37 = ResidualBlock2(1024, 256)
-        self.resblock38 = ResidualBlock2(1024, 256)
-        self.resblock39 = ResidualBlock2(1024, 256)
-        self.resblock310 = ResidualBlock2(1024, 256)
-        self.resblock311 = ResidualBlock2(1024, 256)
-        self.resblock312 = ResidualBlock2(1024, 256)
-        self.resblock313 = ResidualBlock2(1024, 256)
-        self.resblock314 = ResidualBlock2(1024, 256)
-        self.resblock315 = ResidualBlock2(1024, 256)
-        self.resblock316 = ResidualBlock2(1024, 256)
-        self.resblock317 = ResidualBlock2(1024, 256)
-        self.resblock318 = ResidualBlock2(1024, 256)
-        self.resblock319 = ResidualBlock2(1024, 256)
-        self.resblock320 = ResidualBlock2(1024, 256)
-        self.resblock321 = ResidualBlock2(1024, 256)
-        self.resblock322 = ResidualBlock2(1024, 256)
-        self.resblock323 = ResidualBlock2(1024, 256)
-        self.resblock41 = ResidualBlock2(1024, 512)
-        self.resblock42 = ResidualBlock2(2048, 512)
-        self.resblock43 = ResidualBlock2(2048, 512)
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
-    def forward(self, x):
-        pr = self.maxpool(self.relu(self.bn1(self.conv1(x))))
-        b1 = self.resblock13(self.resblock12(self.resblock11(pr)))
-        b2 = self.resblock24(self.resblock23(
-            self.resblock22(self.resblock21(b1))))
-        b3 = self.resblock36(self.resblock35(self.resblock34(
-            self.resblock33(self.resblock32(self.resblock31(b2))))))
-        b4 = self.resblock312(self.resblock311(self.resblock310(
-            self.resblock39(self.resblock38(self.resblock37(b3))))))
-        b5 = self.resblock318(self.resblock317(self.resblock316(
-            self.resblock315(self.resblock314(self.resblock313(b4))))))
-        b6 = self.resblock323(self.resblock322(
-            self.resblock321(self.resblock320(self.resblock319(b5)))))
-        b7 = self.resblock43(self.resblock42(self.resblock41(b6)))
-        b8 = self.avgpool(b7)
-        return b8
+        self.layer1 = _make_layer(ResidualBlock2, ((1, 64, 64),
+                                                   (2, 256, 64)))
 
-    def out_features(self):
-        return self.features
+        self.layer2 = _make_layer(ResidualBlock2, ((1, 256, 128),
+                                                   (3, 512, 128)))
+
+        self.layer3 = _make_layer(ResidualBlock2, ((1, 512, 256),
+                                                   (5, 1024, 256)))
+
+        self.layer4 = _make_layer(ResidualBlock2, ((1, 1024, 512),
+                                                   (2, 2048, 512)))
 
 
-class ResNet152(nn.Module):
+class ResNet101(BaseResNet):
     def __init__(self, inc):
-        super().__init__()
+        super().__init__(inc)
         self.features = 2048
-        self.conv1 = nn.Conv2d(inc, 64, kernel_size=(
-            7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(
-            kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
-        self.resblock11 = ResidualBlock2(64, 64)
-        self.resblock12 = ResidualBlock2(256, 64)
-        self.resblock13 = ResidualBlock2(256, 64)
-        self.resblock21 = ResidualBlock2(256, 128)
-        self.resblock22 = ResidualBlock2(512, 128)
-        self.resblock23 = ResidualBlock2(512, 128)
-        self.resblock24 = ResidualBlock2(512, 128)
-        self.resblock25 = ResidualBlock2(512, 128)
-        self.resblock26 = ResidualBlock2(512, 128)
-        self.resblock27 = ResidualBlock2(512, 128)
-        self.resblock28 = ResidualBlock2(512, 128)
-        self.resblock31 = ResidualBlock2(512, 256)
-        self.resblock32 = ResidualBlock2(1024, 256)
-        self.resblock33 = ResidualBlock2(1024, 256)
-        self.resblock34 = ResidualBlock2(1024, 256)
-        self.resblock35 = ResidualBlock2(1024, 256)
-        self.resblock36 = ResidualBlock2(1024, 256)
-        self.resblock37 = ResidualBlock2(1024, 256)
-        self.resblock38 = ResidualBlock2(1024, 256)
-        self.resblock39 = ResidualBlock2(1024, 256)
-        self.resblock310 = ResidualBlock2(1024, 256)
-        self.resblock311 = ResidualBlock2(1024, 256)
-        self.resblock312 = ResidualBlock2(1024, 256)
-        self.resblock313 = ResidualBlock2(1024, 256)
-        self.resblock314 = ResidualBlock2(1024, 256)
-        self.resblock315 = ResidualBlock2(1024, 256)
-        self.resblock316 = ResidualBlock2(1024, 256)
-        self.resblock317 = ResidualBlock2(1024, 256)
-        self.resblock318 = ResidualBlock2(1024, 256)
-        self.resblock319 = ResidualBlock2(1024, 256)
-        self.resblock320 = ResidualBlock2(1024, 256)
-        self.resblock321 = ResidualBlock2(1024, 256)
-        self.resblock322 = ResidualBlock2(1024, 256)
-        self.resblock323 = ResidualBlock2(1024, 256)
-        self.resblock324 = ResidualBlock2(1024, 256)
-        self.resblock325 = ResidualBlock2(1024, 256)
-        self.resblock326 = ResidualBlock2(1024, 256)
-        self.resblock327 = ResidualBlock2(1024, 256)
-        self.resblock328 = ResidualBlock2(1024, 256)
-        self.resblock329 = ResidualBlock2(1024, 256)
-        self.resblock330 = ResidualBlock2(1024, 256)
-        self.resblock331 = ResidualBlock2(1024, 256)
-        self.resblock332 = ResidualBlock2(1024, 256)
-        self.resblock333 = ResidualBlock2(1024, 256)
-        self.resblock334 = ResidualBlock2(1024, 256)
-        self.resblock335 = ResidualBlock2(1024, 256)
-        self.resblock336 = ResidualBlock2(1024, 256)
-        self.resblock41 = ResidualBlock2(1024, 512)
-        self.resblock42 = ResidualBlock2(2048, 512)
-        self.resblock43 = ResidualBlock2(2048, 512)
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
-    def forward(self, x):
-        pr = self.maxpool(self.relu(self.bn1(self.conv1(x))))
-        b1 = self.resblock13(self.resblock12(self.resblock11(pr)))
-        b2 = self.resblock28(self.resblock27(self.resblock26(self.resblock25(
-            self.resblock24(self.resblock23(
-                self.resblock22(self.resblock21(b1))))))))
-        b3 = self.resblock36(self.resblock35(self.resblock34(
-            self.resblock33(self.resblock32(self.resblock31(b2))))))
-        b4 = self.resblock312(self.resblock311(self.resblock310(
-            self.resblock39(self.resblock38(self.resblock37(b3))))))
-        b5 = self.resblock318(self.resblock317(self.resblock316(
-            self.resblock315(self.resblock314(self.resblock313(b4))))))
-        b6 = self.resblock324(self.resblock323(self.resblock322(
-            self.resblock321(self.resblock320(self.resblock319(b5))))))
-        b7 = self.resblock330(self.resblock329(self.resblock328(
-            self.resblock327(self.resblock326(self.resblock325(b6))))))
-        b8 = self.resblock336(self.resblock335(self.resblock334(
-            self.resblock333(self.resblock332(self.resblock331(b7))))))
-        b9 = self.resblock43(self.resblock42(self.resblock41(b8)))
-        b10 = self.avgpool(b9)
-        return b10
+        self.layer1 = _make_layer(ResidualBlock2, ((1, 64, 64),
+                                                   (2, 256, 64)))
 
-    def out_features(self):
-        return self.features
+        self.layer2 = _make_layer(ResidualBlock2, ((1, 256, 128),
+                                                   (3, 512, 128)))
+
+        self.layer3 = _make_layer(ResidualBlock2, ((1, 512, 256),
+                                                   (22, 1024, 256)))
+
+        self.layer4 = _make_layer(ResidualBlock2, ((1, 1024, 512),
+                                                   (2, 2048, 512)))
+
+
+class ResNet152(BaseResNet):
+    def __init__(self, inc):
+        super().__init__(inc)
+        self.features = 2048
+
+        self.layer1 = _make_layer(ResidualBlock2, ((1, 64, 64),
+                                                   (2, 256, 64)))
+
+        self.layer2 = _make_layer(ResidualBlock2, ((1, 256, 128),
+                                                   (7, 512, 128)))
+
+        self.layer3 = _make_layer(ResidualBlock2, ((1, 512, 256),
+                                                   (35, 1024, 256)))
+
+        self.layer4 = _make_layer(ResidualBlock2, ((1, 1024, 512),
+                                                   (2, 2048, 512)))
